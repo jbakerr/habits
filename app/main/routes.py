421ebfc6a8233c9fcdeb36b9b60e3479182ecf47
Baker Renneckar <bakerrenneckar@gmail.com>
2019-02-13 20:21:39 -0800

    added _check_status route for loading ; update _complete and _undo routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,7 +14,9 @@ def before_request():
     if current_user.is_anonymous == False:
         habbits = Habbit.query.filter_by(creator=current_user)
         for habbit in habbits:
-            habbit_history = HabbitHistory.query.filter_by(habbit_id=habbit.id).order_by(desc(HabbitHistory.timestamp)).first()
+            habbit_history = HabbitHistory.query.filter_by(
+                habbit_id=habbit.id).order_by(
+                desc(HabbitHistory.timestamp)).first()
             if habbit_history is not None:
                 if habbit_history.timestamp.date() < date.today():
                     habbit.active_today = True
@@ -29,7 +31,8 @@ def before_request():
 def index():
     user = current_user
     habbits = Habbit.query.filter_by(user_id = current_user.id)
-    return render_template('index.html', title='Home', user=user, habbits=habbits)
+    return render_template(
+        'index.html', title='Home', user=user, habbits=habbits)
 
 
 @bp.route('/user/<username>')
@@ -46,7 +49,10 @@ def new_habbit():
     user = current_user
     form = NewHabbitForm()
     if form.validate_on_submit():
-        habbit = Habbit(habbit=form.habbit.data, creator=current_user, weekly_goal=form.weekly_goal.data)
+        habbit = Habbit(
+            habbit=form.habbit.data,
+             creator=current_user,
+              weekly_goal=form.weekly_goal.data)
         db.session.add(habbit)
         db.session.commit()
         flash("Your habbit is saved.")
@@ -73,56 +79,62 @@ def habbit(username, id):
         form.weekly_goal.data = habbit.weekly_goal
 
 
-    return render_template('habbit.html', title='Habbit', habbit=habbit, form=form, habbit_history=habbit_history)
+    return render_template(
+        'habbit.html', title='Habbit', habbit=habbit,
+         form=form, habbit_history=habbit_history)
 
 
-
+@login_required
 @bp.route('/_complete', methods=['GET', 'POST'])
 def complete():
     id = request.form['id']
-    print(id)
+    weekly_count = int(request.form['weekly_count'])
     habbit = Habbit.query.filter_by(id=id).first_or_404()
     habbit_history = HabbitHistory.query.filter_by(habbit_id=id)
 
     Habbit.complete_habbit(habbit,current_user)
-    habbit.update_streak(habbit_history)
+    weekly_count = habbit.increase_streak(weekly_count)
     db.session.commit()
 
-    weekly_prog_percent = (habbit.current_streak / habbit.weekly_goal) * 100
 
     return jsonify(
         current_streak=habbit.current_streak,
          longest_streak=habbit.longest_streak,
-         weekly_prog_percent=weekly_prog_percent)
+         weekly_count=weekly_count)
 
-
+@login_required
 @bp.route('/_undo', methods=['GET', 'POST'])
 def undo():
     id = request.form['id']
+    weekly_count = int(request.form['weekly_count'])
     habbit = Habbit.query.filter_by(id=id).first_or_404()
     habbit_history = HabbitHistory.query.filter_by(
         habbit_id=id).order_by(desc(HabbitHistory.timestamp)).first()
     db.session.delete(habbit_history)
-    if habbit.current_streak == habbit.longest_streak:
-        habbit.current_streak -= 1
-        habbit.longest_streak -= 1
-    else:
-        habbit.current_streak -= 1
+    weekly_count = habbit.decrease_streak(weekly_count)
+
     habbit.active_today = True
     db.session.commit()
 
-    weekly_prog_percent = (habbit.current_streak / habbit.weekly_goal) * 100
-
-
-
-    # habbit = Habbit.query.filter_by(id=id).first_or_404()
-    # habbit_history = HabbitHistory.query.filter_by(habbit_id=id)
-
-    # Habbit.complete_habbit(habbit,current_user)
-    # habbit.update_streak(habbit_history)
-    # print(habbit.current_streak)
-    # db.session.commit()
     return jsonify(
         current_streak=habbit.current_streak,
         longest_streak=habbit.longest_streak,
-        weekly_prog_percent=weekly_prog_percent)
+        weekly_count=weekly_count)
+
+@login_required
+@bp.route('/_check_status', methods=['GET', 'POST'])
+def check_status():
+    if current_user.is_anonymous:
+        pass
+    habbits = Habbit.query.filter_by(user_id=current_user.id)
+    json = {}
+    for habbit in habbits:
+        habbit_history = HabbitHistory.query.filter_by(
+            habbit_id=habbit.id).order_by(desc(
+                HabbitHistory.timestamp)).limit(7)
+        history = [status.timestamp.isoformat() for status in habbit_history]
+        if len(history) > 0:
+            json.update({str(habbit.id): history})
+    print(json)
+
+    return jsonify(json)
