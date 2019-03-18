e2cfb42840bfb00be402ebc0a9e406ffb79e8d96
Baker Renneckar <bakerrenneckar@gmail.com>
2019-03-18 15:11:54 -0700

    Cleaned up testing configuration

--- conftest.py
+++ conftest.py
@@ -3,12 +3,11 @@ from config import Config
 import pytest
 
 
-
 # basedir = os.path.abspath(os.path.dirname(__file__))
 # load_dotenv(os.path.join(basedir, '.env'))
+
 
 @pytest.fixture()
 def app():
     app = create_app(Config)
     return app
-
