edc082e897ba6aa052256f32b24baaa4c4fd9971
Baker Renneckar <bakerrenneckar@gmail.com>
2019-04-04 10:58:24 -0700

    updating testing

--- test/test_routes.py
+++ test/test_routes.py
@@ -4,5 +4,15 @@ def test_home_page(test_client):
     WHEN the '/' page is requested (GET)
     THEN check the response is valid
     """
-    response = test_client.get("/")
-    assert response.status_code == 302
+    response = test_client.get("/", follow_redirects=True)
+    assert response.status_code == 200
+
+
+def test_login(test_client, init_database):
+
+    response = test_client.post(
+        "/login",
+        form=dict(username="John@gmail.com", password="password"),
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
